7bbc73815748563226242b9da5f5322c316cb4f9
seonguk_kang <dnr2144@gmail.com>
2024-12-20 17:15:53 +0900

    Add basic domain

---
+++ src/main/java/com/sixsense/newsfeed/domain/User.java
@@ -0,0 +1,33 @@
+package com.sixsense.newsfeed.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "user")
+@Entity
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "age", nullable = false)
+    private Integer age;
+
+}
