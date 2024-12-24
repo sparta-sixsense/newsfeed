952b3d74ad3cda593c1fb3c256d6c40832359550
dllll2 <wlsdud8658@gmail.com>
2024-12-24 19:48:29 +0900

    ✨ feat: ADD ordering by updatedAt in Post query

--- src/main/java/com/sixsense/newsfeed/repository/PostRepository.java
+++ src/main/java/com/sixsense/newsfeed/repository/PostRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     // 사용자 ID로 게시글 조회 + 페이징
-    @Query("SELECT p FROM Post p WHERE p.status = 'ACTIVE'")
+    @Query("SELECT p FROM Post p WHERE p.status = 'ACTIVE' ORDER BY p.updatedAt DESC")
     Page<Post> findAllByUserId(Long userId, Pageable pageable);
 
     // 모든 게시글 조회 + 페이징
