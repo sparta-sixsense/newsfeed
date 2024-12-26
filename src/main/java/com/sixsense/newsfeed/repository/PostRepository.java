759fd1027ca6a23bd7d08069b91f3d87e24dfac7
dllll2 <wlsdud8658@gmail.com>
2024-12-26 09:15:19 +0900

    :sparkles: feat: add query to fetch posts by followed users :bug: refactor: Add parameter to query for filtering posts by userId

--- src/main/java/com/sixsense/newsfeed/repository/PostRepository.java
+++ src/main/java/com/sixsense/newsfeed/repository/PostRepository.java
@@ -5,14 +5,24 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     // 사용자 ID로 게시글 조회 + 페이징
-    @Query("SELECT p FROM Post p WHERE p.status = 'ACTIVE' ORDER BY p.updatedAt DESC")
-    Page<Post> findAllByUserId(Long userId, Pageable pageable);
+    @Query("SELECT p FROM Post p WHERE p.user.id = :userId AND p.isDeleted = false ORDER BY p.updatedAt DESC")
+    Page<Post> findAllByUserId(@Param("userId") Long userId, Pageable pageable);
 
     // 모든 게시글 조회 + 페이징
     Page<Post> findAllByOrderByUpdatedAtDesc(Pageable pageable);
+
+    // 팔로우 하고있는 사람들 게시글 조회
+    @Query("SELECT p FROM Post p " +
+            "JOIN FollowRelationship fr ON p.user.id = fr.following.id " +
+            "WHERE fr.follower.id = :userId " +
+            "AND p.isDeleted = false")
+    Page<Post> findPostsByUsersFollowedBy(@Param("userId") Long userId, Pageable pageable);
 
 }
