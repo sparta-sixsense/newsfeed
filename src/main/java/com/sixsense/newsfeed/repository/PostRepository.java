1e08d0dec3e07ee5d4a5fbe0bc135cc69a09b87b
dllll2 <wlsdud8658@gmail.com>
2024-12-26 12:00:10 +0900

    ✨ feat: Add swagger annotations

--- src/main/java/com/sixsense/newsfeed/repository/PostRepository.java
+++ src/main/java/com/sixsense/newsfeed/repository/PostRepository.java
@@ -7,8 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface PostRepository extends JpaRepository<Post, Long> {
 
     // 사용자 ID로 게시글 조회 + 페이징
