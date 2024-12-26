98bff5e3160f5ef8a3ce8dde26a7772414a8739a
seonguk_kang <dnr2144@gmail.com>
2024-12-26 14:47:11 +0900

    ✨feature: Add api info '/signup', '/login'

--- src/main/java/com/sixsense/newsfeed/controller/UserApiController.java
+++ src/main/java/com/sixsense/newsfeed/controller/UserApiController.java
@@ -17,6 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import static com.sixsense.newsfeed.constant.Token.AUTHORIZATION_HEADER;
 
 @RequiredArgsConstructor
@@ -26,21 +27,57 @@ public class UserApiController {
 
     private final UserService userService;
 
-    @Autowired
-    private TokenProvider tokenProvider; // 필드 주입
 
+    @Operation(
+            summary = "회원가입",
+            description = "회원가입을 진행합니다.",
+            tags = {"Profile Management"}
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "회원가입이 성공하면, 가입된 정보 확인용으로 반환",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+    )
     @PostMapping("/signup")
-    public ResponseEntity<SignUpResponseDto> createUser(@Valid @RequestBody SignUpRequestDto requestDto) {
-        log.info("/signup");
+    public ResponseEntity<SignUpResponseDto> createUser(@Valid
+                                                        @RequestBody
+                                                        @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                                                                description = "가입에 필요한 회원정보",
+                                                                required = true,
+                                                                content = @Content(
+                                                                        mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                                                        schema = @Schema(implementation = SignUpRequestDto.class)
+                                                                )
+                                                        )
+                                                        SignUpRequestDto requestDto) {
 
         SignUpResponseDto response = userService.save(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(response);
     }
 
+    @Operation(
+            summary = "로그인",
+            description = "로그인을 진행합니다.",
+            tags = {"Profile Management"}
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "로그인에 성공하면, 'AccessToken'과 'RefreshToken'을 반환합니다.",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE)
+    )
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDto> login(@Valid @RequestBody LoginRequestDto requestDto) {
-        log.info("/login");
+    public ResponseEntity<LoginResponseDto> login(@Valid
+                                                  @RequestBody
+                                                  @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                                                          description = "",
+                                                          required = true,
+                                                          content = @Content(
+                                                                  mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                                                  schema = @Schema(implementation = LoginRequestDto.class)
+                                                          )
+                                                  )
+                                                  LoginRequestDto requestDto) {
 
         LoginResponseDto response = userService.authenticate(requestDto);
         return ResponseEntity.ok()
